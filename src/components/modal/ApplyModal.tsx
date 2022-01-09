293eab4c189fe0dac2424146346f79329ddb4ba2
sangwooeom <cdm19720@gmail.com>
2022-01-09 20:49:50 +0900

    Textarea 컴포넌트 생성

---
+++ src/components/modal/ApplyModal.tsx
@@ -0,0 +1,8 @@
+import cn from 'classnames';
+import styles from 'css/components/modal/ApplyModal.module.scss';
+
+export default function ApplyModal() {
+    return (
+        <div></div>
+    )
+}
