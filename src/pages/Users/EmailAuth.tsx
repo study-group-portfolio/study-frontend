dca124296e98c19f57d6000cea9fe147efcb46ab
Donghoon Han <apensia@Donghoons-MacBook-Pro.local>
2022-01-10 18:59:56 +0900

    Email Auth below text added

--- src/pages/Users/EmailAuth.tsx
+++ src/pages/Users/EmailAuth.tsx
@@ -1,9 +1,6 @@
 import cn from "classnames";
 import { Link } from "react-router-dom";
 import styles from "../../css/pages/users/Users.module.scss";
-import { ReactComponent as Kakao } from "../../images/img/img_social_kakao.svg";
-import { ReactComponent as Google } from "../../images/img/img_social_google.svg";
-import BackBtn from "../../images/arrow_back_btn_blue.svg";
 
 export default function EmailAuth() {
   return (
@@ -15,6 +12,16 @@ export default function EmailAuth() {
             인증 메일을 <strong>example@studyit.com</strong>(으)로 전송했습니다.
             <br />
             받으신 이메일을 열어 버튼을 클릭하면 회원가입이 완료됩니다.
+          </p>
+        </div>
+        <div className={cn(styles.errorExpSection)}>
+          <p className={cn(styles.errorExp)}>
+            인증 메일을 받지 못하셨나요?
+            <br />
+            스팸보관함 확인 또는{" "}
+            <Link to="/email-auth" className={cn(styles.emailResendBtn)}>
+              인증 메일 재전송
+            </Link>
           </p>
         </div>
       </div>
