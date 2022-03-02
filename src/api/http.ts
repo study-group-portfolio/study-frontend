85a46d6494e1e543650e099ea3cc674c33fa1a6d
Donghoon Han <apensia@Donghoons-MacBook-Pro.local>
2022-03-02 21:52:25 +0900

    Signup validation finished & redirect unvalid url

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,63 +1,65 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { store, persistStor } from 'store/store';
-import jwt_decode from 'jwt-decode';
-import moment from 'moment';
-import { getReissueAccessToken } from './userAPI';
-import { editLogin } from 'store/login/state';
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { store } from "store/store";
+import jwt_decode from "jwt-decode";
+import moment from "moment";
+import { getReissueAccessToken } from "./userAPI";
+import { editLogin } from "store/login/state";
 
 export interface Token {
-    exp: number;
-    iat: number;
-    id?: number;
-    iss: string;
-    role: string;
-    sub: string;
+  exp: number;
+  iat: number;
+  id?: number;
+  iss: string;
+  role: string;
+  sub: string;
 }
 
 const HTTP: AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 10000,
-    withCredentials: false,
-    headers: {
-        "content-Type": "application/json",
-    }
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 10000,
+  withCredentials: false,
+  headers: {
+    "content-Type": "application/json",
+  },
 });
 
 HTTP.interceptors.request.use(
-    async function(config: AxiosRequestConfig) {
-        const { login } = store.getState();
+  async function (config: AxiosRequestConfig) {
+    const { login } = store.getState();
 
-        if (login.isLogin) {
-            const decodeAccessToken: Token = jwt_decode(login.accessToken);
-            const decodeRefreshToken: Token = jwt_decode(login.refreshToken);
+    if (login.isLogin) {
+      const decodeAccessToken: Token = jwt_decode(login.accessToken);
+      const decodeRefreshToken: Token = jwt_decode(login.refreshToken);
 
-            const currentDate = moment();
-            const accessTokenExp = new Date(decodeAccessToken.exp * 1000);
-            const refreshTokenExp = new Date(decodeRefreshToken.exp * 1000);
+      const currentDate = moment();
+      const accessTokenExp = new Date(decodeAccessToken.exp * 1000);
+      const refreshTokenExp = new Date(decodeRefreshToken.exp * 1000);
 
-            if (config && config.headers && login.accessToken) {
-                config.headers['Authorization'] = `Bearer ${login.accessToken}`;
-            }
+      if (config && config.headers && login.accessToken) {
+        config.headers["Authorization"] = `Bearer ${login.accessToken}`;
+      }
 
-            if (!currentDate.isBefore(accessTokenExp)) {
-                if (currentDate.isBefore(refreshTokenExp)) {
-                    const { data: { accessToken } } = await getReissueAccessToken();
-                    store.dispatch(editLogin(accessToken, login.refreshToken));
-                    if (config && config.headers && login.accessToken) {
-                        config.headers['Authorization'] = `Bearer ${accessToken}`;
-                    }
-                } else {
-                    window.location.href = "/users/login";
-                }
-            }
+      if (!currentDate.isBefore(accessTokenExp)) {
+        if (currentDate.isBefore(refreshTokenExp)) {
+          const {
+            data: { accessToken },
+          } = await getReissueAccessToken();
+          store.dispatch(editLogin(accessToken, login.refreshToken));
+          if (config && config.headers && login.accessToken) {
+            config.headers["Authorization"] = `Bearer ${accessToken}`;
+          }
+        } else {
+          window.location.href = "/users/login";
         }
-
-        return config;
-    },
-    function(error: any) {
-        console.error(`api interceptor request error: ${error}`);
-        return Promise.reject(error);
+      }
     }
+
+    return config;
+  },
+  function (error: any) {
+    console.error(`api interceptor request error: ${error}`);
+    return Promise.reject(error);
+  }
 );
 
 export default HTTP;
